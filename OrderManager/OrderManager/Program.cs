5bcab1e8ea75d84c348c6b735412a7ca29fd0cd2
Ваше Имя <nikolaevaanastasia940@gmail.com>
2026-04-25 16:26:40 +0300

    OrderManager: added implementation

---
+++ OrderManager/OrderManager/Program.cs
@@ -0,0 +1,79 @@
+﻿class OrderManager
+{
+    private record DataOrder(string Name, string Product, int Count, string Address);
+
+    public static void Main()
+    {
+        DataOrder order = RequestDataFromUser();
+
+        if (!IsOrderAgreed(order))
+        {
+            Console.WriteLine("Заказ отменён");
+            return;
+        }
+        const int numberDaysDelivery = 3;
+        DateTime dateReceipt = DateTime.Today.AddDays(numberDaysDelivery);
+        Console.WriteLine($"\n{order.Name}! Ваш заказ {order.Product} в количестве {order.Count} " +
+            $"оформлен! Ожидайте доставку по адресу {order.Address} к {dateReceipt}");
+    }
+
+    private static bool IsOrderAgreed(DataOrder order)
+    {
+        Console.WriteLine($"\nЗдравствуйте, {order.Name}, вы заказали {order.Count} " +
+            $"{order.Product} на адрес {order.Address}, все верно?(y/n)");
+
+        string answer = (Console.ReadLine() ?? "").ToLower();
+        while (!(answer == "y" || answer == "n"))
+        {
+            Console.WriteLine("Все верно?(y/n)");
+            answer = (Console.ReadLine() ?? "").ToLower();
+        }
+        return answer == "y";
+    }
+    private static DataOrder RequestDataFromUser()
+    {
+        Console.WriteLine("Введите имя пользователя:");
+        string name = GetInputString();
+
+        Console.WriteLine("Введите название товара:");
+        string product = GetInputString();
+
+        Console.WriteLine("Введите количество товара:");
+        int count = GetInputInt();
+
+        Console.WriteLine("Введите aдрес доставки:");
+        string address = GetInputString();
+
+        return new DataOrder(name, product, count, address);
+    }
+
+    private static int GetInputInt()
+    {
+        string input = Console.ReadLine() ?? "";
+        int num;
+        while (string.IsNullOrWhiteSpace(input)
+            || !int.TryParse(input, out num)
+            || num <= 0)
+        {
+            Console.WriteLine("Вы должны ввести число, которое будет больше 0.");
+            input = Console.ReadLine() ?? "";
+        }
+        return num;
+    }
+
+    private static string GetInputString()
+    {
+        string input = Console.ReadLine() ?? "";
+        while (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Ввод не может быть пустым.");
+            input = Console.ReadLine() ?? "";
+        }
+        return DeleteExtraSpaces(input);
+    }
+
+    private static string DeleteExtraSpaces(string str)
+    {
+        return string.Join(" ", str.Split(' ', StringSplitOptions.RemoveEmptyEntries));
+    }
+}

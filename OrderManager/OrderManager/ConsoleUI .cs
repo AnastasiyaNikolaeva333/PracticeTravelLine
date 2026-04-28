947830b04f76308885d67476f876e9a6e06a6484
Anastasiya Nikolaeva <nikolaevaanastasia940@gmail.com>
2026-04-28 15:15:49 +0300

    OrderManager: правки по ревью(вынесла некоторую логику по отдельным классам, исправила написание констант

---
+++ OrderManager/OrderManager/ConsoleUI .cs
@@ -0,0 +1,49 @@
+﻿internal class ConsoleUI
+{
+    private const string AnswerYes = "y";
+    private const string AnswerNo = "n";
+    public static void RequestDataFromUser( Order order )
+    {
+        Console.WriteLine( "Введите имя пользователя:" );
+        order.Name = DeleteExtraSpaces( Console.ReadLine() );
+
+        Console.WriteLine( "Введите название товара:" );
+        order.Product = DeleteExtraSpaces( Console.ReadLine() );
+
+        Console.WriteLine( "Введите количество товара:" );
+        order.ProductQuantity = GetInputInt();
+
+        Console.WriteLine( "Введите aдрес доставки:" );
+        order.Address = DeleteExtraSpaces( Console.ReadLine() );
+    }
+    private static int GetInputInt()
+    {
+        string input = DeleteExtraSpaces( Console.ReadLine() );
+        int cout;
+        while ( string.IsNullOrWhiteSpace( input ) || !int.TryParse( input, out cout ) )
+        {
+            Console.WriteLine( "Пункт количества товара должен быть числом" );
+            input = DeleteExtraSpaces( Console.ReadLine() );
+        }
+        return cout;
+    }
+    private static string DeleteExtraSpaces( string str )
+    {
+        str = string.Join( " ", str.Split( ' ', StringSplitOptions.RemoveEmptyEntries ) );
+        return str.Trim();
+    }
+
+    public static bool IsOrderAgreed( Order order )
+    {
+        Console.WriteLine( $"\nЗдравствуйте, {order.Name}, вы заказали {order.ProductQuantity} " +
+            $"{order.Product} на адрес {order.Address}, все верно?(y/n)" );
+
+        string answer = ( Console.ReadLine() ).ToLower();
+        while ( !( answer == AnswerYes || answer == AnswerNo ) )
+        {
+            Console.WriteLine( "Все верно?(y/n)" );
+            answer = ( Console.ReadLine() ).ToLower();
+        }
+        return answer == AnswerYes;
+    }
+}
---
+++ OrderManager/OrderManager/Order.cs
@@ -0,0 +1,40 @@
+﻿internal class Order
+{
+    private string name;
+    private string product;
+    private int productQuantity;
+    private string address;
+
+    public string Name
+    {
+        get => name;
+        set => name = GoodValue( value, "Имя" );
+    }
+    public string Product
+    {
+        get => product;
+        set => product = GoodValue( value, "Название товара" );
+    }
+    public int ProductQuantity
+    {
+        get => productQuantity;
+        set
+        {
+            productQuantity = value > 0
+                ? value
+                : throw new ArgumentException( "Количество товара должно быть больше 0" );
+        }
+    }
+    public string Address
+    {
+        get => address;
+        set => address = GoodValue( value, "Адресс" );
+    }
+
+    private string GoodValue( string value, string identifier )
+    {
+        return !string.IsNullOrWhiteSpace( value )
+            ? value
+            : throw new ArgumentException( $"{identifier} не может быть пустым" );
+    }
+}

38559c938495c3c0958e46beccbb34b3f5c328e0
Anastasiya Nikolaeva <nikolaevaanastasia940@gmail.com>
2026-05-11 11:18:44 +0300

    Fighters: inaccuracies in the text have been fixed

--- Fighters/Fighters/Character.cs
+++ Fighters/Fighters/Character.cs
@@ -13,6 +13,36 @@
         return fighter;
 
     }
+    private static IRace GetRace()
+    {
+        List<IRace> races = new List<IRace>()
+        {
+            new Human(),
+            new Dragon(),
+            new Avatar(),
+            new Beastmen()
+        };
+        Console.WriteLine( "Выберите расу из списка ниже:" );
+        for ( int i = 1; i <= races.Count; i++ )
+        {
+            Console.WriteLine( $"{i}. {races[ i ].Name}" );
+        }
+        string race = Response.GetResponseFromUser();
+        HashSet<string> allRace = new HashSet<string> { "1", "2", "3", "4" };
+        while ( !allRace.Contains( race ) )
+        {
+            Console.WriteLine( "Вы должны ввести число от 1-4" );
+            race = Response.GetResponseFromUser();
+        }
+        switch ( race )
+        {
+            case "0": return races[ 0 ];
+            case "1": return races[ 1 ];
+            case "2": return races[ 2 ];
+            case "3": return races[ 3 ];
+            default: throw new ArgumentException();
+        }
+    }
     private static IRole GetRole()
     {
         List<IRole> roles = new List<IRole>()
@@ -24,15 +54,15 @@
 
         };
         Console.WriteLine( "Выберете роль персонажу" );
-        for ( int i = 0; i < roles.Count; i++ )
+        for ( int i = 1; i <= roles.Count; i++ )
         {
             Console.WriteLine( $"{i}. {roles[ i ].Name}" );
         }
         string role = Response.GetResponseFromUser();
-        HashSet<string> allRoles = new HashSet<string> { "0", "1", "2", "3" };
+        HashSet<string> allRoles = new HashSet<string> { "1", "2", "3", "4" };
         while ( !allRoles.Contains( role ) )
         {
-            Console.WriteLine( "Вы должны ввести число от 0-4" );
+            Console.WriteLine( "Вы должны ввести число от 1-4" );
             role = Response.GetResponseFromUser();
         }
         switch ( role )
@@ -74,7 +104,7 @@
         HashSet<string> allArmor = new HashSet<string> { "0", "1", "2", "3" };
         while ( !allArmor.Contains( armor ) )
         {
-            Console.WriteLine( "Вы должны ввести число от 0-4" );
+            Console.WriteLine( "Вы должны ввести число от 0-3" );
             armor = Response.GetResponseFromUser();
         }
         switch ( armor )
@@ -116,36 +146,6 @@
             case "2": { fighter.SetWeapon( weapons[ 2 ] ); break; }
             case "3": { fighter.SetWeapon( weapons[ 3 ] ); break; }
             case "4": { fighter.SetWeapon( weapons[ 4 ] ); break; }
-            default: throw new ArgumentException();
-        }
-    }
-    private static IRace GetRace()
-    {
-        List<IRace> races = new List<IRace>()
-        {
-            new Human(),
-            new Dragon(),
-            new Avatar(),
-            new Beastmen()
-        };
-        Console.WriteLine( "Выберите расу из списка ниже:" );
-        for ( int i = 0; i < races.Count; i++ )
-        {
-            Console.WriteLine( $"{i}. {races[ i ].Name}" );
-        }
-        string race = Response.GetResponseFromUser();
-        HashSet<string> allRace = new HashSet<string> { "0", "1", "2", "3" };
-        while ( !allRace.Contains( race ) )
-        {
-            Console.WriteLine( "Вы должны ввести число от 0-3" );
-            race = Response.GetResponseFromUser();
-        }
-        switch ( race )
-        {
-            case "0": return races[ 0 ];
-            case "1": return races[ 1 ];
-            case "2": return races[ 2 ];
-            case "3": return races[ 3 ];
             default: throw new ArgumentException();
         }
     }
